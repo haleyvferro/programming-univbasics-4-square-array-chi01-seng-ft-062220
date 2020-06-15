c51c9b32ec154135d5420b813e2c8854027f13f1
Haley Ferro <haleyvferro@gmail.com>
2020-06-15 02:25:51 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,9 @@
-numbers = [1,2,3]
-new_numbers = []
+# numbers = [1,2,3]
+# new_numbers = []
 
- def square_array(numbers)
-   while numbers do |index|
-  new_array.push(array[index] ** 2)
-end
-square array
- end
+# def square_array(numbers)
+#   while numbers do |index|
+#   new_array.push(array[index] ** 2)
+# end
+# square array
+# end
