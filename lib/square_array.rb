8b09ad8cf86472147765a2e06a6f38ccfe0866aa
Haley Ferro <haleyvferro@gmail.com>
2020-06-15 02:08:34 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,25 +1,15 @@
-#array = [1,2,3,4,5]
-#new_array = []
- 
-#array.length.times do |index|
-#  new_array.push(array[index] + 1)
-#end
-#new_array
-
-#  count = 0
- # new_array = []
-  #while count < array.length do
-   # new_array.push(array[count].to_s) # OR new_array << #array[count].to_s
-    #count += 1
-  #end
-  #new_array
-
 numbers = [1,2,3,]
 new_numbers = []
  
-def square_array
-  while count < numbers.length do
-   new_numbers.push(numbers[count].to_s)
-   count ** 2 
- end
- new_numbers
+ numbers.length.times do |index|
+  new_numbers.push(numbers[index] ** 2)
+end
+new_array
+ 
+ 
+#def square_array
+ # while count < numbers.length do
+  # new_numbers.push(numbers[count].to_s)
+   #count ** 2 
+ #end
+# new_numbers
